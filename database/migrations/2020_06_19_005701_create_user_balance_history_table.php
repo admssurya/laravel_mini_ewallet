af69cbd0ee7fc234f0e61cd53530e51f4ac8f495
Adimas Surya <adms.surya@gmail.com>
2020-06-19 08:08:39 +0700

    feature-add-migration-user-balance-history-table

---
+++ database/migrations/2020_06_19_005701_create_user_balance_history_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBalanceHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_balance_history', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_balance_id');
+            $table->integer('balance_before');
+            $table->integer('balance_after');
+            $table->string('activity');
+            $table->enum('type', ['credit','debit']);
+            $table->string('ip');
+            $table->string('location');
+            $table->string('user_agent');
+            $table->string('author');
+            $table->timestampsTz();
+            $table->softDeletesTz();
+
+            $table->foreign('user_balance_id')
+                ->references('id')
+                ->on('user_balances')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_balance_history');
+    }
+}
