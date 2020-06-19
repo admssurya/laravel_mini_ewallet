3dfc55ef04f9cc2d61be500f549c9e3c2a7a0bd3
Adimas Surya <adms.surya@gmail.com>
2020-06-19 07:56:38 +0700

    feature-add-migration-user-balance-table

---
+++ database/migrations/2020_06_19_005048_create_user_balance_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBalanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_balances', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->integer('balance');
+            $table->integer('balance_achieve');
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_balances', function (Blueprint $table) {
+            $table->dropIfExists();
+        });
+    }
+}
