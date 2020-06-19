17c2d7fe88c8faa5fe897b50ec4a5795ef254552
Adimas Surya <adms.surya@gmail.com>
2020-06-19 14:34:08 +0700

    feature-add-user-balance-history-transformer

---
+++ app/Transformers/UserBalanceHistoryTransformer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\UserBalanceHistory;
+use League\Fractal\TransformerAbstract;
+
+class UserBalanceHistoryTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @param UserBalanceHistory $userBalanceHistory
+     * @return array
+     */
+    public function transform(UserBalanceHistory $userBalanceHistory)
+    {
+        return [
+            'balance_before' => $userBalanceHistory->balance_before,
+            'balance_after' => $userBalanceHistory->balance_after,
+            'activity' => $userBalanceHistory->activity,
+            'type' => $userBalanceHistory->type,
+            'ip' => $userBalanceHistory->ip,
+            'location' => $userBalanceHistory->location,
+            'user_agent' => $userBalanceHistory->user_agent,
+            'author' => $userBalanceHistory->author
+        ];
+    }
+}
