e7959b728c96c94c55748fd2824fc54b1d0d877c
Adimas Surya <adms.surya@gmail.com>
2020-06-19 09:39:04 +0700

    feature-fix-user-seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +15,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\User::class, 1)->create();
+        $this->truncate();
+
+        User::create([
+            'name' => 'Administrator',
+            'username' => 'administrator',
+            'email' => 'administrator@mail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        factory(User::class, 3)->create();
+    }
+
+    public function truncate()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('users')->truncate();
+        Schema::enableForeignKeyConstraints();
     }
 }
